44c23e7979a2c9d4c264c1584f9e1e8624748b62
cnzau <clement.nzau@gmail.com>
2016-04-28 20:17:29 +0300

    import ast for conversion from string, used raw input for user to enter data

--- pythonData.py
+++ pythonData.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import ast
 """Accepts as the first parameter a string specifying the data structure to be used 'list', 'set' or 'dictionary'
 Accepts the second parameter the data to be manipulated based on the data structure specified e.g [1, 4, 9, 16, 25] for a list data structure
 Based on the first parameter
@@ -6,20 +7,24 @@ Based on the first parameter
   -add items in the global variables to the set and return the resulting set
   -return the keys of a dictionary
 """
+st = raw_input('Enter type of data(list, set or dictionary): ')
+dt = raw_input('Enter data to be manipulated: ')
+# st = ast.literal_eval(st)
+dt = ast.literal_eval(dt)
 
 m = 'MORINGA'
 h = 'IHUB'
 l = 'ILAB'
-def manipulate_data(string, data):
-    if string == "list":
+def manipulate_data(sti, data):
+    if sti == "list":
         data.reverse()
         return data
-    elif string == "set":
+    elif sti == "set":
         data.add(m)
         data.add(h)
         data.add(l)
         return data
-    elif string == "dictionary":
+    elif sti == "dictionary":
         return data.keys()
     else:
         return 'data type not a list, set or a dictionary'
